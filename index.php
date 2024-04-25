6b3adca3cf661fb0914bd92781b2e1b28bd38323
Jasper <jaspergatila2@gmail.com>
2024-04-25 08:06:58 +0800

    updating the index and adding favicon to the index file

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Online Tech Store</title>
 
-
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="../Online_Tech_Store_v1/assets/Favicon/icons8-online-shop-96.png" type="image/x-icon">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
   <!-- Bootstrap CSS -->
